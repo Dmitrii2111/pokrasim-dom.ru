4ffecc41e3fa18d0eac34fa35b3f9a664f7843e7
D.Hrulev <dmitryhrulev@gmail.com>
2021-03-18 17:45:04 +0300

    Header + firstScren + Form + OurCases

---
+++ src/JS/pure_js.js
@@ -0,0 +1,10 @@
+let back = [
+   'url(./img//Back_Case/безцветный.jpg)',
+   'url(./img//Back_Case/дом-из-дерева-белого-цвета.jpg)',
+   'url(./img//Back_Case/дом-из-темного-бруса.jpg)',
+   'url(./img//Back_Case/коричневый.jpg',
+   'url(./img//Back_Case/красный.jpg)',
+   'url(./img//Back_Case/серый.jpg)'
+]
+
+
--- src/index.js
+++ src/index.js
@@ -1,1 +1,2 @@
 import './style.scss'
+import './JS/pure_js'
